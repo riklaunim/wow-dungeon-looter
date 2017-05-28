52ce4083c4e4f272e9ddebdbc21be5ac775ea711
Piotr Maliński <riklaunim@gmail.com>
2017-05-28 20:37:09 +0200

    Refactor specialisations.

---
+++ looter/app/models/drop-sources.js
@@ -0,0 +1,15 @@
+import LeatherUsers from '../../models/specialisations';
+
+
+export default [
+  {
+    'name': 'Cour of stars',
+    'lootTable': [
+      {
+        'name': 'Footpads of the Swift Balestra', 'slot': 'feet',
+        'dropsFor': LeatherUsers,
+        'critical': 750, 'mastery': 443, 'primary': 1483,
+      }
+    ],
+  }
+];
--- looter/app/models/specialisations.js
+++ looter/app/models/specialisations.js
@@ -1,32 +1,41 @@
+export const BloodDK = {
+  'name': 'Blood DK',
+  'label': 'bloodDK',
+};
+export const FrostDK = {
+  'name': 'Frost DK',
+  'label': 'frostDK',
+};
+export const UnholyDK = {
+  'name': 'Unholy DK',
+  'label': 'unholyDK',
+  'types': ['dps', 'melee-dps'],
+};
+export const Brewmaster = {
+  'name': 'Brewmaster Monk',
+  'label': 'brewmaster',
+  'types': ['tank'],
+};
+export const Mistweaver = {
+  'name': 'Mistweaver Monk',
+  'label': 'mistweaver',
+  'types': ['healer'],
+};
+export const Windwalker = {
+  'name': 'Windwalker Monk',
+  'label': 'windwalker',
+  'types': ['dps', 'melee-dps'],
+};
+
+export const LeatherUsers = [Brewmaster, Mistweaver, Windwalker];
+export const MailUsers = [];
+export const PlateUsers = [BloodDK, FrostDK, UnholyDK];
+
 export default [
-  {
-    'name': 'Blood DK',
-    'label': 'bloodDK',
-    'types': ['tank'],
-  },
-  {
-    'name': 'Frost DK',
-    'label': 'frostDK',
-    'types': ['dps', 'melee-dps'],
-  },
-  {
-    'name': 'Unholy DK',
-    'label': 'unholyDK',
-    'types': ['dps', 'melee-dps'],
-  },
-  {
-    'name': 'Brewmaster Monk',
-    'label': 'brewmaster',
-    'types': ['tank'],
-  },
-  {
-    'name': 'Mistweaver Monk',
-    'label': 'mistweaver',
-    'types': ['healer'],
-  },
-  {
-    'name': 'Windwalker Monk',
-    'label': 'windwalker',
-    'types': ['dps', 'melee-dps'],
-  }
+  BloodDK,
+  FrostDK,
+  UnholyDK,
+  Brewmaster,
+  Mistweaver,
+  Windwalker
 ];
