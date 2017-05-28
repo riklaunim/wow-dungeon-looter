ee05b55cada0040a339a3e12d36488eb455a86b5
Piotr Maliński <riklaunim@gmail.com>
2017-05-28 14:01:07 +0200

    Add results controller.

---
+++ looter/tests/unit/controllers/settings/results-test.js
@@ -0,0 +1,12 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('controller:settings/results', 'Unit | Controller | settings/results', {
+  // Specify the other units that are required for this test.
+  needs: ['controller:settings']
+});
+
+// Replace this with your real tests.
+test('it exists', function(assert) {
+  let controller = this.subject();
+  assert.ok(controller);
+});
