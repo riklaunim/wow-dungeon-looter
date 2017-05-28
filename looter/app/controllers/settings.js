f4ab07fcc4ea3d5d1ee25da62151d3c9cfc99ecd
Piotr Maliński <riklaunim@gmail.com>
2017-05-28 13:49:56 +0200

    Set current stat weights.

--- looter/app/controllers/settings.js
+++ looter/app/controllers/settings.js
@@ -5,9 +5,17 @@ import Specialisations from '../models/specialisations';
 export default Ember.Controller.extend({
   specialisations: Specialisations,
   currentSpecialisation: 'bloodDK',
+  weightPrimary: 1,
+  weightCritical: 1,
+  weightHaste: 1,
+  weightMastery: 1,
+  weightVersatility: 1,
   actions: {
     selectSpecialisation(specialisation) {
       this.set('currentSpecialisation', specialisation);
+    },
+    showResults() {
+      this.transitionToRoute('settings.results');
     }
   }
 });
